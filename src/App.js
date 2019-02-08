029db7ebf4da536264023f76e6b2486275929772
HuguesRomain <huguesromainpro@gmail.com>
2019-02-08 20:56:05 +0100

    functional recpise

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import Header from './components/Header'
 import recettes from './recettes'; 
 import Admin from './components/Admin';
+import Card from './components/Card'
 
 class App extends Component {
   state = {
@@ -14,13 +15,13 @@ class App extends Component {
   loadExample = () => this.setState({recettes})
 
   render () {
+    const cards = Object.keys(this.state.recettes)
+    .map(key => <Card key={key} details={this.state.recettes[key]}></Card>)
     return (
       <div className='box'>
        <Header pseudo={this.state.pseudo}></Header>
         <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
+          {cards}
         </div>
         <Admin
         loadExample={this.loadExample}></Admin>
