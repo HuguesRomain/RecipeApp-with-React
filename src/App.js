82789ac2bee9658b497017ce60736ac3a7d79197
HuguesRomain <huguesromainpro@gmail.com>
2019-02-08 19:27:36 +0100

    import recipe

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 // CSS
 import './App.css'
 import Header from './components/Header'
+import recettes from './recettes'; 
+import Admin from './components/Admin';
 
 class App extends Component {
   state = {
-    pseudo: this.props.match.params.pseudo
+    pseudo: this.props.match.params.pseudo,
+    recettes: {}
   }
+
+  loadExample = () => this.setState({recettes})
 
   render () {
     return (
@@ -17,6 +22,8 @@ class App extends Component {
             <h2>Une Carte</h2>
           </div>
         </div>
+        <Admin
+        loadExample={this.loadExample}></Admin>
       </div>
     )
   }
---
+++ src/components/Admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Admin extends Component {
+  render() {
+    return(
+      <footer>
+      <button 
+      onClick={this.props.loadExample}>Remplir</button>
+      </footer>
+    )
+  }
+}
+
+export default Admin
